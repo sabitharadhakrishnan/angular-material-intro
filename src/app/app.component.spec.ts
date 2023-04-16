f4034e61947905bf8decd80d0c2a7e4445824608
Sabitha Radhakrishnan <sabitharadhakrishnan@gmail.com>
2023-04-16 09:17:56 -0500

    adding icons and buttons

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatButtonModule,
+        MatIconModule
       ],
       declarations: [
         AppComponent
@@ -26,10 +30,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-material-intro');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-material-intro app is running!');
-  });
+
 });
